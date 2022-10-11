f59050a6638badf783a67a716c8299c1f1e86feb
Nellykhris <ifeomaomeonu@gmail.com>
2022-10-11 22:30:59 +0100

    Air BnB

---
+++ test.py
@@ -0,0 +1,27 @@
+# string = '"67"'
+# # # args = string.replace(':', 'a')
+# print(string.strip("'"))
+# print((string.strip("'")).isdigit())
+
+
+# a_list = ['me', 'you', 'us', 'they']
+# for i in a_list[1:]:
+#     print(i)
+
+#
+# classes = {
+#                'BaseModel': 'Value_BaseModel', 'User': 'Value_User', 'Place': 'Value_Place',
+#                'State': 'Value_State', 'City': 'Value_City', 'Amenity': 'Value_Amenity',
+#                'Review': 'Value_Review'
+#               }
+#
+# del classes['BaseModel']
+# print(classes)
+# # if 'BaseModel' in classes:
+#     print('True')
+# else:
+#     print('False')
+
+
+_list = {1: 'me'}, {2, 'you'}, {3, 'us'}
+print(_list)
